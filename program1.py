90e421a734d7d853f2d62ccde6c2fee417674926
amitkumarisdevelopeer <130381253+amitkumarisdevelopeer@users.noreply.github.com>
2024-09-25 15:38:10 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -4,17 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in mapping.values():  # If it's an opening bracket
+                stack.append(char)
+            elif char in mapping.keys():  # If it's a closing bracket
+                if not stack or stack.pop() != mapping[char]:
+                    return False
+                
+        return not stack  # True if no unmatched opening brackets are left
 
-
-
-
-
-
-
-    
-
-
-
-  
 
